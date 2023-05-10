f9bcd512ee5ff680fe02c101696b4441c6415264
farukh <faruk.hopic@gmail.com>
2023-05-10 23:22:37 +0200

    Add order item observer and register in app service provider

---
+++ app/Observers/OrderItemObserver.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\OrderItem;
+
+class OrderItemObserver
+{
+    /**
+     * Handle the OrderItem "created" event.
+     */
+    public function created(OrderItem $orderItem): void
+    {
+        $product = $orderItem->product;
+        $quantity = $orderItem->quantity;
+        // Calculate total product availability and total order item price when order item is created
+        // Todo: check if availability total count is negative
+        $orderItem->update([
+            'price' => $product->calculateTotalPrice($quantity),
+        ]);
+        $product->update([
+            'availability' => $product->availability - $quantity
+        ]);
+    }
+
+    /**
+     * Handle the OrderItem "updated" event.
+     */
+    public function updated(OrderItem $orderItem): void
+    {
+        //
+    }
+
+    /**
+     * Handle the OrderItem "deleted" event.
+     */
+    public function deleted(OrderItem $orderItem): void
+    {
+        //
+    }
+
+    /**
+     * Handle the OrderItem "restored" event.
+     */
+    public function restored(OrderItem $orderItem): void
+    {
+        //
+    }
+
+    /**
+     * Handle the OrderItem "force deleted" event.
+     */
+    public function forceDeleted(OrderItem $orderItem): void
+    {
+        //
+    }
+}
